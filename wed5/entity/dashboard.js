044b691670373f43b2766b5f3d234358b8a6c998
huynguyen2617 <huy.nv151697@sis.hust.edu.vn>
2021-10-14 12:07:29 +0700

    tao moi flie

---
+++ wed5/entity/dashboard.js
@@ -0,0 +1,13 @@
+export class board{
+    constructor(stx, sty, enx, eny, style, ctx){
+        this.stx =stx;
+        this.sty= sty;
+        this.enx =enx;
+        this.eny= eny;
+        this.style = style;
+        this.ctx= ctx;   
+    }
+    draw(){
+        this.ctx.
+    }
+}
---
+++ wed5/main1.js
@@ -0,0 +1,6 @@
+import{paper} from "./entity/paper.js";
+import{board} from "./entity/dashboard.js";
+const canvas =document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const paper
